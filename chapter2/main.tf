c521604b4d26720013ed770abb4fd3eb51d05883
harora-loves-tech <77118762+harora-loves-tech@users.noreply.github.com>
2021-03-16 00:00:24 +0530

    Adding Security Group

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "myfirstec2instancewithterraform" {
     ami = "ami-081bb417559035fe8"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.myfirstsecuritygroupwithterraform.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
         Name = "My-First-EC2Instance-With-Terraform"
+    }
+}
+
+resource "aws_security_group" "myfirstsecuritygroupwithterraform" {
+    name = "first-instance-security-group-with-terraform"
+
+    ingress  {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
