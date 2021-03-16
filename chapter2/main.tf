d3383a6b8fae7647078b409419a4fb4f160fd00b
harora-loves-tech <77118762+harora-loves-tech@users.noreply.github.com>
2021-03-16 23:19:19 +0530

    Adding vars and web server

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -2,15 +2,21 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+variable server_port {
+    type = number
+    description = "This represents the port of the server"
+    default = 8080
+} 
+
 resource "aws_instance" "myfirstec2instancewithterraform" {
-    ami = "ami-081bb417559035fe8"
+    ami = "ami-0d758c1134823146a"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.myfirstsecuritygroupwithterraform.id]
 
     user_data = <<-EOF
                 #!/bin/bash
-                echo "Hello World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
     tags = {
@@ -22,9 +28,14 @@ resource "aws_security_group" "myfirstsecuritygroupwithterraform" {
     name = "first-instance-security-group-with-terraform"
 
     ingress  {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output public_ip {
+    value = aws_instance.myfirstec2instancewithterraform.public_ip
+    description = "This is the public ip of the created EC2 instance"
 }
